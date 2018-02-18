10ab05a84727b530c52a6a4ef4cc8bbdc964ef15
Patrick Knight <ptknight00@gmail.com>
2018-02-18 14:10:26 -0600

    updating seeds

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,8 +2,6 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const table = require('console.table');
 
-var ids = [];
-
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -12,7 +10,7 @@ const connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
     start();
 })
@@ -26,31 +24,32 @@ function start() {
         choices: ['Browse all products', 'Browse products by department', 'Exit']
     }).then(answers => {
 
-            if (answers.entry === 'Browse all products') {
-                console.log('browse all');
-                displayAll();
-            } else if (answers.entry === 'Browse products by department'){
-                console.log('browse by dept')
-                displayDept();
-            } else {
-                process.exit();
-            }})
+        if (answers.entry === 'Browse all products') {
+            console.log('browse all');
+            displayAll();
+        } else if (answers.entry === 'Browse products by department') {
+            console.log('browse by dept')
+            displayDept();
+        } else {
+            process.exit();
         }
+    })
+}
 
 function displayAll() {
-    connection.query('SELECT item_id, department_name, product_name, price FROM products WHERE stock_quantity>1', function(err, res) {
+    connection.query('SELECT item_id, department_name, product_name, price FROM products WHERE stock_quantity>1', function (err, res) {
         if (err) throw err;
         console.table(res);
-        res.forEach((prod) => {
-            ids.push(prod.id)
-        });
-        console.log(ids);
+        // res.forEach((prod) => {
+        //     ids.push(prod.id)
+        // });
+        // console.log(ids);
         select();
     })
 }
 
 function displayDept() {
-    connection.query('SELECT department_name FROM products', function(err, res) {
+    connection.query('SELECT department_name FROM products', function (err, res) {
         if (err) throw err;
         console.table(res);
     })
@@ -58,35 +57,36 @@ function displayDept() {
 
 function select() {
     inquirer.prompt([{
-        name: 'select',
-        type: 'input',
-        message: 'Please enter the id of the item you wish to purchase'  
-    }, 
-    {
-        name: 'quantity',
-        type: 'input',
-        message: 'Please enter the quantity you would like to purchase'
-    }
+            name: 'select',
+            type: 'input',
+            message: 'Please enter the id of the item you wish to purchase'
+        },
+        {
+            name: 'quantity',
+            type: 'input',
+            message: 'Please enter the quantity you would like to purchase'
+        }
     ]).then(answers => {
 
-    connection.query(`SELECT product_name, price, stock_quantity FROM products WHERE item_id=${answers.select}`, function(err, res){
-        var stock = res[0].stock_quantity
-        console.log(stock);
-        if (err) throw err;
-        if (stock >= answers.quantity) {
-            connection.query(`UPDATE products SET stock_quantity=stock_quantity-? where ?`, [answers.quantity, {item_id: answers.select}], function(err){
-                if (err) throw err;
-                console.log(`Order Summary: \nThank You! You have successfully ordered a total of ${answers.quantity} $${res[0].product_name}, for a total cost of ${res[0].price * answers.quantity}`);
+        connection.query(`SELECT product_name, price, stock_quantity FROM products WHERE item_id=${answers.select}`, function (err, res) {
+            var stock = res[0].stock_quantity
+            if (err) throw err;
+            if (stock >= answers.quantity) {
+                connection.query(`UPDATE products SET stock_quantity=stock_quantity-? where ?`, [answers.quantity, {
+                    item_id: answers.select
+                }], function (err) {
+                    if (err) throw err;
+                    console.log(`Order Summary: \nThank You! You have successfully ordered a total of ${answers.quantity} $${res[0].product_name}, for a total cost of ${res[0].price * answers.quantity}`);
+                    keepShopping();
+                })
+            } else {
+                console.log('Sorry, order could not be fulfilled in this quantity.');
                 keepShopping();
-            })
-        } else {
-            console.log('Sorry, order could not be fulfilled in this quantity.');
-            keepShopping();
-            
-        }
-    })
 
-})
+            }
+        })
+
+    })
 }
 
 function keepShopping() {
@@ -95,7 +95,7 @@ function keepShopping() {
         type: 'confirm',
         message: 'Would you like to continue shopping?'
     }).then(conf => {
-        if (conf.continue){
+        if (conf.continue) {
             displayAll();
         } else {
             console.log('Thank you for shopping with Bamazon');
--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,6 +1,16 @@
 use bamazon;
 
 insert into products(product_name, department_name, price, stock_quantity) values ('Insidious (2011) Blu-ray', 'Movies & TV', 13.99, 20);
+insert into products(product_name, department_name, price, stock_quantity) values ('Unsolved Mysteries (1988) Complete Series', 'Movies & TV', 76.88, 15);
+insert into products(product_name, department_name, price, stock_quantity) values ('Bamboo 14" Cooking Chopsticks', 'Home & Kitchen', 6.89, 30);
+insert into products(product_name, department_name, price, stock_quantity) values ('Monster (2003) Blu-ray', 'Movies & TV', 9.99, 20);
+insert into products(product_name, department_name, price, stock_quantity) values ('Yakami Orchard 100% Pure Japanese Yuzu Juice', 'Grocery & Gormet Foods', 11.99, 10);
+insert into products(product_name, department_name, price, stock_quantity) values ('A Girl and Her Pig: Recipes and Stories by April Bloomfield', 'Books', 18.35, 12);
+insert into products(product_name, department_name, price, stock_quantity) values ('Cocktail Kingdom Yarai Mixing Glass 500ml', 'Home & Kitchen', 32.99, 10);
+insert into products(product_name, department_name, price, stock_quantity) values ('Momofuku by David Chang (2009)', 'Books', 26.49, 12);
+insert into products(product_name, department_name, price, stock_quantity) values ('Heritage by Sean Brock (2014)', 'Books', 22.09, 12);
+insert into products(product_name, department_name, price, stock_quantity) values ('Shun Premier 7" Santoku Knife', 'Home & Kitchen', 167.95, 5);
+insert into products(product_name, department_name, price, stock_quantity) values ('Schott Zwiesel Pure Bordeaux Red Wine Glass (6)', 'Home & Kitchen', 83.95, 4);
 insert into products(product_name, department_name, price, stock_quantity) values ('Fender Road Worn® 60s Jazzmaster®', 'Musical Instruments', 1299.00, 3);
 
 select * from bamazon.products;
